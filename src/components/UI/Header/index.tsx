79b5bd58f0304b26c6ef9a84d7f85023fae5a778
ricci2511 <ricardo.christmann@protonmail.com>
2022-09-19 12:32:33 +0200

    ci: replaced theme select with a button instead

--- src/components/UI/Header/index.tsx
+++ src/components/UI/Header/index.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import { CgMenuRightAlt } from 'react-icons/cg';
-import ThemeSwitchSelect from '@components/UI/buttons/theme-switch-select';
+import ThemeSwitchButton from '@components/UI/buttons/theme-switch-button';
 
 const Header = ({ withDrawerBtn = false }: { withDrawerBtn?: boolean }) => {
     return (
         <>
             <header className="navbar border-b border-b-base-100 bg-base-200">
-                <div className="flex-none">
-                    {withDrawerBtn && (
+                {withDrawerBtn && (
+                    <div className="flex-none">
                         <label
                             htmlFor="my-drawer-2"
                             className="btn btn-ghost drawer-button lg:hidden"
+                            aria-label="open sidebar menu"
                         >
                             <CgMenuRightAlt size={22} />
                         </label>
-                    )}
-                </div>
+                    </div>
+                )}
                 <div className="flex-1 px-3">
                     <a className="text-2xl font-bold normal-case">OpenKBan</a>
                 </div>
-                <div className="px-3">
-                    <ThemeSwitchSelect />
+                <div className="flex-none px-3">
+                    <ThemeSwitchButton />
                 </div>
             </header>
         </>
