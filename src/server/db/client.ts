53bd66fd31370de3374cc7dc15dbe085b42be989
ricci2511 <ricardo.christmann@protonmail.com>
2022-12-23 19:08:42 +0100

    fix: properly updated packages and modified prisma client to follow best practices

--- src/server/db/client.ts
+++ src/server/db/client.ts
@@ -1,15 +1,11 @@
 import { PrismaClient } from '@prisma/client';
 
-declare global {
-    // allow global `var` declarations
-    // eslint-disable-next-line no-var
-    var prisma: PrismaClient | undefined;
-}
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
-    global.prisma ||
+    globalForPrisma.prisma ||
     new PrismaClient({
         log: ['query'],
     });
 
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
