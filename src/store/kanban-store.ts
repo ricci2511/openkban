6965b62f2aa6c9a5d7f3da33a9de5b87037808b5
ricci2511 <ricardo.christmann@protonmail.com>
2023-02-09 13:02:45 +0100

    ci: added immer middleware to kanban zustand store

--- src/store/kanban-store.ts
+++ src/store/kanban-store.ts
@@ -2,6 +2,7 @@ import { sortByLexoRankAsc } from '@lib/lexorank-helpers';
 import { BoardTask } from '@prisma/client';
 import { BoardColumnWithTasks } from 'types/board-types';
 import { create } from 'zustand';
+import { immer } from 'zustand/middleware/immer';
 
 export type ColumnTasks = {
     [columnId: string]: BoardColumnWithTasks;
@@ -19,69 +20,46 @@ const initColumnTasks = (columns: BoardColumnWithTasks[]) => {
     );
 };
 
-const addTask = (task: BoardTask, columns: ColumnTasks) => {
-    const columnId = task.columnId;
-    return {
-        ...columns,
-        [columnId]: {
-            ...columns[columnId],
-            tasks: [...columns[columnId].tasks, task],
-        },
-    };
-};
-
-const deleteTask = (task: BoardTask, columns: ColumnTasks) => {
-    const columnId = task.columnId;
-    return {
-        ...columns,
-        [columnId]: {
-            ...columns[columnId],
-            tasks: columns[columnId].tasks.filter((t) => t.id !== task.id),
-        },
-    };
-};
-
 type KanbanStore = {
     columnTasks: ColumnTasks;
     setColumnTasks: (columnTasks: ColumnTasks) => void;
     initColumnTasks: (columns: BoardColumnWithTasks[]) => void;
     addColumn: (column: BoardColumnWithTasks) => void;
+    updateColumnColor: (id: string, color: string) => void;
     addTask: (task: BoardTask) => void;
     deleteTask: (task: BoardTask) => void;
 };
 
-const useKanbanStore = create<KanbanStore>(
-    (set): KanbanStore => ({
+const useKanbanStore = create(
+    immer<KanbanStore>((set) => ({
         columnTasks: {},
         setColumnTasks: (columnTasks: ColumnTasks) =>
-            set((state) => ({
-                ...state,
-                columnTasks,
-            })),
+            set((state) => {
+                state.columnTasks = columnTasks;
+            }),
         initColumnTasks: (columns: BoardColumnWithTasks[]) =>
-            set((state) => ({
-                ...state,
-                columnTasks: initColumnTasks(columns),
-            })),
+            set((state) => {
+                state.columnTasks = initColumnTasks(columns);
+            }),
         addColumn: (column: BoardColumnWithTasks) =>
-            set((state) => ({
-                ...state,
-                columnTasks: {
-                    ...state.columnTasks,
-                    [column.id]: column,
-                },
-            })),
+            set((state) => {
+                state.columnTasks[column.id] = column;
+            }),
+        updateColumnColor: (id: string, color: string) =>
+            set((state) => {
+                state.columnTasks[id].color = color;
+            }),
         addTask: (task: BoardTask) =>
-            set((state) => ({
-                ...state,
-                columnTasks: addTask(task, state.columnTasks),
-            })),
+            set((state) => {
+                state.columnTasks[task.columnId].tasks.push(task);
+            }),
         deleteTask: (task: BoardTask) =>
-            set((state) => ({
-                ...state,
-                columnTasks: deleteTask(task, state.columnTasks),
-            })),
-    })
+            set((state) => {
+                state.columnTasks[task.columnId].tasks = state.columnTasks[
+                    task.columnId
+                ].tasks.filter((t) => t.id !== task.id);
+            }),
+    }))
 );
 
 export default useKanbanStore;
