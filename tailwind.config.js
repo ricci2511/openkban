16f8a251b855040907d268fcd4ebb3b29f067ce0
ricci2511 <ricardo.christmann@protonmail.com>
2023-03-01 13:28:13 +0100

    fix: added missing tw config content to prevent tw from purging daisyui styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    content: [
+        './src/**/*.{js,jsx,ts,tsx}',
+        'node_modules/daisyui/dist/**/*.js',
+        'node_modules/react-daisyui/dist/**/*.js',
+    ],
     theme: {
         extend: {},
     },
