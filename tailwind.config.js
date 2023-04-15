330d4555da8f914fd118648aec812ebdb7756377
ricci2511 <ricardo.christmann@protonmail.com>
2023-04-15 02:09:25 +0200

    chore: removed react-daisyui package

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,6 @@ module.exports = {
     content: [
         './src/**/*.{js,jsx,ts,tsx}',
         'node_modules/daisyui/dist/**/*.js',
-        'node_modules/react-daisyui/dist/**/*.js',
     ],
     theme: {
         extend: {},
